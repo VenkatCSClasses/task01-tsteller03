940bb4c264f9a82ea488d24d850a74af509f7d08
tsteller03 <tsteller03@icloud.com>
2026-02-04 21:12:30 -0500

    Imported my isEmailValid function originally written for my partner's project onto my own

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -72,56 +72,112 @@ public class BankAccount {
 
 
     public static boolean isEmailValid(String email){
-        if (email.indexOf('@') == -1
-                || email.indexOf('@') != email.lastIndexOf('@')
-                || email.indexOf('@') == 0
-                || email.indexOf('@') == email.length() - 1
-                || email.indexOf(' ') != -1
-                || email.indexOf('.') == -1
-                || email.indexOf('.') == 0
-                || email.indexOf('.') == email.length() - 1
-                || email.indexOf('.') == email.indexOf('@') - 1
-                || email.indexOf('.') == email.indexOf('@') + 1
-                || email.lastIndexOf('.') >= email.length() - 2
-                || email.indexOf("..") != -1
-                || email.indexOf('#') != -1
-                || email.indexOf('!') != -1
-                || email.indexOf('$') != -1
-                || email.indexOf('%') != -1
-                || email.indexOf('^') != -1
-                || email.indexOf('&') != -1
-                || email.indexOf('*') != -1
-                || email.indexOf('(') != -1
-                || email.indexOf(')') != -1
-                || email.indexOf('+') != -1
-                || email.indexOf('=') != -1
-                || email.indexOf('{') != -1
-                || email.indexOf('}') != -1
-                || email.indexOf('[') != -1
-                || email.indexOf(']') != -1
-                || email.indexOf('~') != -1
-                || email.indexOf('`') != -1
-                || email.indexOf("'") != -1
-                || email.indexOf('"') != -1
-                || email.indexOf('?') != -1
-                || email.indexOf('-') == email.indexOf('@') - 1
-                || email.indexOf('-') == email.indexOf('@') + 1
-                || email.indexOf('-') == email.length() - 1
-                || email.indexOf('-') == 0
-                || email.indexOf("--") != -1
-                || email.indexOf('_') == email.indexOf('@') - 1
-                || email.indexOf('_') == email.indexOf('@') + 1
-                || email.indexOf('_') == email.length() - 1
-                || email.indexOf('_') == 0
-                || email.indexOf("__") != -1
-                || email.substring(email.indexOf('@')).indexOf('.') == -1
-                || email.equals(null)
-                )
-                {
+        //must not be blank
+        if (email == null || email.equals("")) {
             return false;
         }
-        else {
-            return true;
+
+        //must have one and only one @ symbol
+        int atCount = 0;
+        for (int i = 0; i < email.length(); i++) {
+            if (email.charAt(i) == '@') {
+                atCount++;
+            }
         }
+
+        if (atCount != 1) {
+            return false;
+        }
+
+        //split prefix and domain for specific checking
+        int atSymbol = email.indexOf('@');
+        String prefix = "";
+        String domain = "";
+
+        for (int i = 0; i < email.length(); i++) {
+            if (i < atSymbol) {
+                prefix += email.charAt(i);
+            } else if (i > atSymbol) {
+                domain += email.charAt(i);
+            }
+        }
+
+        //prefix and domain must not be empty
+        if (prefix.length() == 0 || domain.length() == 0) {
+            return false;
+        }
+        
+        //no ".." anywhere
+        for (int i = 0; i < email.length() - 1; i++) {
+            if (email.charAt(i) == '.' && email.charAt(i + 1) == '.') {
+                return false;
+            }
+        }
+
+        //no "--" anywhere
+        for (int i = 0; i < email.length() - 1; i++) {
+            if (email.charAt(i) == '-' && email.charAt(i + 1) == '-') {
+                return false;
+            }
+        }
+
+        //no "__" anywhere
+        for (int i = 0; i < email.length() - 1; i++) {
+            if (email.charAt(i) == '_' && email.charAt(i + 1) == '_') {
+                return false;
+            }
+        }
+
+        //prefix start & end character rules (., -, _ can't be first or last)
+        char prefixFirst = prefix.charAt(0);
+        char prefixLast = prefix.charAt(prefix.length() - 1);
+        if (prefixFirst == '.' || prefixFirst == '-' || prefixFirst == '_' ||
+            prefixLast == '.' || prefixLast == '-' || prefixLast == '_') {
+            return false;
+        }
+
+        //domain start & end character rules (., -, _, @ can't be first or last)
+        char domainFirst = domain.charAt(0);
+        char domainLast = domain.charAt(domain.length() - 1);
+        if (domainFirst == '.' || domainFirst == '-' || domainFirst == '_' || domainFirst == '@' ||
+            domainLast == '.' || domainLast == '-' || domainLast == '_' || domainLast == '@') {
+            return false;
+        }
+
+        //must have at least two characters following the dot in the domain
+        int lastDot = -1;
+        for (int i = 0; i < domain.length(); i++) {
+            if (domain.charAt(i) == '.') {
+                lastDot = i;
+            }
+        }
+
+        if (lastDot == -1 || domain.length() - lastDot - 1 < 2) {
+            return false;
+        }
+
+        //validate characters in prefix, ensure no special characters
+        for (int i = 0; i < prefix.length(); i++) {
+            char current = prefix.charAt(i);
+            if ((current < 'a' || current > 'z') &&
+                (current < 'A' || current > 'Z') &&
+                (current < '0' || current > '9') &&
+                (current != '.' && current != '_' && current != '-')) {
+                    return false;
+                }
+        }
+
+        //validate characters in domain, ensure no special characters
+        for (int i = 0; i < domain.length(); i++) {
+            char current = domain.charAt(i);
+            if ((current < 'a' || current > 'z') &&
+                (current < 'A' || current > 'Z') &&
+                (current < '0' || current > '9') &&
+                (current != '.' && current != '_' && current != '-')) {
+                    return false;
+                }
+        }
+
+        return true;
     }
 }
